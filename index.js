f3abd4aa1d9c4495e681b73d1c92e61931215ada
victor gabriel <gabriel.vides96@gmail.com>
2024-06-19 15:50:38 +0200

    Some test with fs

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-const express = require('express'); 
-const morgan = require('morgan')
+import express from 'express'
+import morgan from 'morgan'
+import fs from 'node:fs'
 
-
-const PORT = process.env.PORT || 3000;
-const app = express();
+// Constants or Configs //
+const PORT = process.env.PORT ?? 3000
+const app = express()
 
 // Settings oder Configs //
-app.use(morgan('combined'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(express.urlencoded())
 
-// app.METHOD(PATH, HANDLER) // 
-
+// app.METHOD(PATH, HANDLER) //
 app.get('/', function (req, res) {
-    res.send('Hello World!');
-    });
+  res.sendFile('index.html', { root: './' })
+})
 
-app.listen(
-    PORT,
-    () => console.log(`Server is running on http://localhost:${PORT}`)
-);
+app.post('/register', function (req, res) {
+  const { name, email, password } = req.body
 
+  const htmlStream =
+    fs.readFileSync('register.html', 'utf-8', (error, data) => {
+      if (error) {
+        console.log('Error:', error)
+        return
+      }
+      console.log('Data:', data)
+      return data
+    })
+  const htmlWithUserData = htmlStream.replace('{{ UserData }}', `Name: ${name}, Email: ${email}, Password: ${password}`)
+
+  res.send(htmlWithUserData)
+})
+
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+)
