339ed2b66c6982df1a7a58854bdd56b7aab58470
victor gabriel <gabriel.vides96@gmail.com>
2024-07-03 04:18:44 +0200

    config cors db scripts and dependencies

--- configs.js
+++ configs.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 
 const PORT = process.env.PORT ?? 3000
 const app = express()
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+
+app.use(cors())
 
 app.use(express.json())
 
---
+++ db.js
@@ -0,0 +1,17 @@
+import sqlite3 from 'sqlite3'
+
+sqlite3.verbose()
+
+const dbName = 'DATABASE.db'
+
+const DATABASE = new sqlite3.Database(dbName, (error) => {
+  if (error) {
+    return console.error('Error connecting to database:', error.message)
+  }
+  const sql = 'CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY NOT NULL, username TEXT NOT NULL, name TEXT, email TEXT UNIQUE NOT NULL, password TEXT NOT NULL)'
+  DATABASE.run(sql, (error) => { if (error) console.error('Error creating table:', error.message) })
+
+  console.info('Connected to SQlite database.')
+})
+
+export { DATABASE }
